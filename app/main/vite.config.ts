ab7cc589e09cb53bdd9634297c8d4df4050be1e4
Asam <www.xiangmaoxian@qq.com>
2022-09-05 17:43:44 +0800

    fix: build gh-pages

--- app/main/vite.config.ts
+++ app/main/vite.config.ts
@@ -10,38 +10,43 @@ import { resolve } from 'path'
 const pathResolve = (path: string) => resolve(__dirname, path)
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    VueJsx(),
-    DefineOptions(),
-    eslintPlugin(),
-    Components({
-      resolvers: [NaiveUiResolver()]
-    })
-  ] as PluginOption[],
-  css: {
-    modules: {
-      localsConvention: 'camelCaseOnly' // 驼峰和横线，文件名引入
-    }
-  },
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: pathResolve('src')
+export default defineConfig(({ mode }) => {
+  // const envOptions = loadEnv(mode, process.cwd())
+  console.log(mode)
+  return {
+    base: './',
+    plugins: [
+      vue(),
+      VueJsx(),
+      DefineOptions(),
+      eslintPlugin(),
+      Components({
+        resolvers: [NaiveUiResolver()]
+      })
+    ] as PluginOption[],
+    css: {
+      modules: {
+        localsConvention: 'camelCaseOnly' // 驼峰和横线，文件名引入
       }
-    ]
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          lodash: ['lodash'],
-          'naive-ui': ['naive-ui'],
-          vue: ['vue'],
-          pinia: ['pinia'],
-          vuedraggable: ['vuedraggable']
+    },
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: pathResolve('src')
+        }
+      ]
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            lodash: ['lodash'],
+            'naive-ui': ['naive-ui'],
+            vue: ['vue'],
+            pinia: ['pinia'],
+            vuedraggable: ['vuedraggable']
+          }
         }
       }
     }
